ec8df853f99be7e57fa8a71c735fba714ea4a1de
Michał Łukasiewicz <michal.lukasiewicz@KQ6X9NYJY4.local>
2025-08-14 22:24:18 +0200

    chore(build): update version metadata for build 1.0.0 (2)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
   version: '1.0.0',
   buildNumber: 2,
   versionCode: 2,
-  buildDate: '2025-08-14T19:38:14.927Z',
-  gitCommit: 'f16008f',
+  buildDate: '2025-08-14T20:22:14.122Z',
+  gitCommit: 'e7cae4e',
   appId: 'com.audio2relation.app',
   appName: 'audio2relations-web-client'
 };
